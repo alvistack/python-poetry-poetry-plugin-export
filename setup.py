d7eea5ec536116fa6c7ecf444ca306f739c0719f
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-05-30 12:03:02 +0800

    alvistack/1.6.0
        git clean -xdf
        tar zcvf ../python-poetry-plugin-export_1.6.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-poetry-plugin-export.spec ../python-poetry-plugin-export_1.6.0-1.spec
        mv ../python*-poetry-plugin-export*1.6.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-poetry-poetry-plugin-export-1.6.0/
        rm -rf ../python*-poetry-plugin-export*1.6.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['poetry_plugin_export']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry-core>=1.7.0,<2.0.0', 'poetry>=1.6.0,<2.0.0']
+
+entry_points = \
+{'poetry.application.plugin': ['export = '
+                               'poetry_plugin_export.plugins:ExportApplicationPlugin']}
+
+setup_kwargs = {
+    'name': 'poetry-plugin-export',
+    'version': '1.6.0',
+    'description': 'Poetry plugin to export the dependencies to various formats',
+    'author': 'Sébastien Eustace',
+    'author_email': 'sebastien@eustace.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://python-poetry.org/',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
